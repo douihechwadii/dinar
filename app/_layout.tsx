12f2ca320994163249d1846aa23505f02289f348
Wadii Douihech <wadiidouihech@gmail.com>
2025-07-17 18:19:17 +0100

    made a simple tab layout with icons

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,51 @@
-import { Stack } from "expo-router";
+import { Octicons } from '@expo/vector-icons';
+import { Tabs } from 'expo-router';
 
-export default function RootLayout() {
-  return <Stack />;
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        tabBarActiveTintColor: '#007aff',
+        tabBarInactiveTintColor: '#888',
+        headerShown: false,
+      }}
+    >
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: 'Home',
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="home" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="income"
+        options={{
+          title: 'Income',
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="arrow-down" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="expense"
+        options={{
+          title: 'Expense',
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="arrow-up" color={color} size={size} />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="settings"
+        options={{
+          title: 'Settings',
+          tabBarIcon: ({ color, size }) => (
+            <Octicons name="gear" color={color} size={size} />
+          ),
+        }}
+      />
+    </Tabs>
+  );
 }
