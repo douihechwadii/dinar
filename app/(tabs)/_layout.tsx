bb313b75d7f05d532df1dad80f7de5b258d1f0dd
Wadii Douihech <wadiidouihech@gmail.com>
2025-07-28 22:32:27 +0100

    added icons to the tabbar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,3 +1,4 @@
+import Ionicons from '@expo/vector-icons/Ionicons';
 import { Tabs } from "expo-router";
 import { View } from "react-native";
 
@@ -5,10 +6,50 @@ export default function TabLayout() {
     return (
         <View style={{ flex: 1, backgroundColor: '#f8f8f8'}}>
             <Tabs screenOptions={{}}>
-                <Tabs.Screen name="index" options={{ title: 'Home'}}/>
-                <Tabs.Screen name="income" options={{ title: 'Income'}}/>
-                <Tabs.Screen name="expense" options={{ title: 'Expense'}}/>
-                <Tabs.Screen name="budget" options={{ title: 'Budget'}}/>
+                <Tabs.Screen name="index" options={{ 
+                    title: 'Home',
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'home' : 'home-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                    }}
+                />
+                <Tabs.Screen name="income" options={{ 
+                    title: 'Income',
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'arrow-down-circle' : 'arrow-down'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                    }}
+                />
+                <Tabs.Screen name="expense" options={{ 
+                    title: 'Expense',
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'arrow-up-circle' : 'arrow-up'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                    }}
+                />
+                <Tabs.Screen name="saving" options={{ 
+                    title: 'Saving',
+                    tabBarIcon: ({ color, size, focused }) => (
+                        <Ionicons
+                            name={focused ? 'card' : 'card-outline'}
+                            size={size}
+                            color={color}
+                        />
+                    )
+                    }}
+                />
             </Tabs>
         </View>
     );
--- app/(tabs)/saving.tsx
+++ app/(tabs)/saving.tsx
@@ -1,6 +1,6 @@
 import { Text, View } from "react-native";
 
-export default function BudgetScreen() {
+export default function SavingScreen() {
   return (
     <View
       style={{
