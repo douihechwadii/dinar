7e656c10a4fb78c45141b9fc034cc6e189fac44e
Wadii Douihech <wadiidouihech@gmail.com>
2025-07-31 13:20:00 +0100

    added the database logic

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,9 +1,16 @@
 import CustomHeader from "@/components/CustomHeader";
+import { initializeDatabase } from "@/database/database";
 import { Stack } from "expo-router";
+import { useEffect } from "react";
 import { StatusBar } from "react-native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function RootLayout() {
+
+  useEffect(() => {
+    initializeDatabase();
+  }, []);
+
   return (
     <SafeAreaProvider>
       <StatusBar barStyle="dark-content"/>
